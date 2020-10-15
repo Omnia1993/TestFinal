b1aec23047f2e2e399b001f3a21764027ca25e26
omnia266 <omniaeid266@gmail.com>
2020-10-15 00:32:46 -0500

    add project

---
+++ Question7/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+
+namespace Question7
+{
+    class Program
+    {
+        static string keyword;
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            string description;
+            Console.WriteLine("enter reserved word: ");
+            keyword = Console.ReadLine();
+                description = CSharpFlashCard(keyword);
+                Console.WriteLine($"Definition for {keyword}: {description}");
+            
+            static String CSharpFlashCard(string reserved)
+            {
+
+
+                switch (reserved.ToLower())
+                {
+
+                    case "switch":
+
+                        return ("used to switching after given spesfic condtion");
+
+                    case "if":
+
+                        return ("used to check spesifc condition");
+                    case "for":
+
+                        return ("used to loop ");
+                    case "int":
+
+                        return ("used to define integer number ");
+                    case "break":
+
+                        return ("used to break out of the loop");
+
+
+                    default:
+                        return ("what");
+
+                }
+
+
+            }
+        }
+    }
+}
